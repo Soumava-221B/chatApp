3abeec1ca1f49c4f5992be079cd6acdeeb860dd0
Soumava Das <soumavadas06@gmail.com>
2025-08-14 16:58:52 +0530

    MongoDB added

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv"
+
+import authRotes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+
+dotenv.config()
+const app = express();
+
+const PORT = process.env.PORT | 5001
+
+app.use("/api/auth", authRotes);
+
+app.listen(PORT, () => {
+    console.log(`server is running on PORT:${PORT}`);
+    connectDB();
+});
