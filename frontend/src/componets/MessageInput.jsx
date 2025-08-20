3f8c4cb046b53de5358e1265431b63b0551d320b
Soumava Das <soumavadas06@gmail.com>
2025-08-20 10:55:50 +0530

    some changes

---
+++ frontend/src/componets/MessageInput.jsx
@@ -0,0 +1,43 @@
+import { X } from "lucide-react";
+import { useState } from "react"
+
+const MessageInput = () => {
+    const [text, setText] = useState("");
+    const [imagePreview, setImagePreview] = useState(null);
+    const fileInputRef = useRef(null);
+
+    const handleImageChange = (e) => {};
+
+    const removeImage = () => {}
+
+    const handleSendMessage = async(e) => {}
+
+  return (
+    <div className="p-4 w-full">
+        {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
+
+      <form onSubmit={handleSendMessage} className=""></form>
+
+    </div>
+  )
+}
+
+export default MessageInput
