4aa733658b8dc7a5c06d85977e2a22f5504e4aaa
Soumava Das <soumavadas06@gmail.com>
2025-09-03 00:57:16 +0530

    auto-scroll

--- frontend/src/componets/ChatContainer.jsx
+++ frontend/src/componets/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
@@ -7,9 +7,16 @@ import { useAuthStore } from "../store/useAtuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const { messages, getMessages, isMessagesLoading, selectedUser, subscribeToMessages, unsubscribeFromMessages } =
-    useChatStore();
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
   const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
@@ -17,7 +24,18 @@ const ChatContainer = () => {
     subscribeToMessages();
 
     return () => unsubscribeFromMessages();
-  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+  }, [
+    selectedUser._id,
+    getMessages,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  ]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   if (isMessagesLoading) {
     return (
@@ -40,6 +58,7 @@ const ChatContainer = () => {
             className={`chat ${
               message.senderId === authUser._id ? "chat-end" : "chat-start"
             }`}
+            ref={messageEndRef}
           >
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
@@ -54,9 +73,9 @@ const ChatContainer = () => {
               </div>
             </div>
             <div className="chat-header mb-1">
-                <time className="text-us opacity-50 ml-1">
-                  {formatMessageTime(message.createdAt)}
-                </time>
+              <time className="text-us opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
             </div>
             <div className="chat-bubble flex flex-col">
               {message.image && (
