b6ac9be772158b055b97d5317bd989ec1cf5b6a5
Soumava Das <soumavadas06@gmail.com>
2025-09-03 21:32:42 +0530

    minor changes for code production

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,40 @@
 import express from "express";
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
-import cors from "cors"
-import { app,server } from "./lib/socket.js";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT | 5001
+const PORT = process.env.PORT | 5001;
+const __dirname = path.resolve();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
-    console.log(`server is running on PORT:${PORT}`);
-    connectDB();
+  console.log(`server is running on PORT:${PORT}`);
+  connectDB();
 });
